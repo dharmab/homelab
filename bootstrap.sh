0d873fc397118cbab45f79d9a0b2e40da85947c4
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2016-01-01 12:28:54 -0700

    Refactor bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,61 +1,48 @@
 #!/bin/bash
 set -e
 
-g_ANSIBLE_REPOSITORY_URL="https://github.com/ansible/ansible.git"
-g_ANSIBLE_CONFIG="/etc/ansible/ansible.cfg"
-g_ANSIBLE_DIRECTORY="/home/vagrant/ansible"
-g_ANSIBLE_BINARY="$g_ANSIBLE_DIRECTORY/bin/ansible"
+set_ansible_config() {
+    section=$1
+    option=$2
+    value=$3
+
+    ansible localhost -i /etc/ansible/hosts -c local -m ini_file -a "dest=/etc/ansible/ansible.cfg section=$section option=$option value=$value"
+}
 
 install_ansible() {
+    ansible_repository_url="https://github.com/ansible/ansible.git"
+    ansible_directory="/home/vagrant/ansible"
+
     # Installing the release version Ansible is the easiest way to get all of
     # the dependencies. Also, we use the release version of Ansible to bootstrap
     # our configuration.
     yum -y install ansible git
+    set_ansible_config defaults host_key_checking False
+    set_ansible_config defaults inventory /vagrant/inventory.ini
+    set_ansible_config defaults remote_user vagrant
 
     # Clone the repository
-    if [ ! -f $g_ANSIBLE_BINARY ]; then
+    if [ ! -d $ansible_directory ]; then
         pushd /home/vagrant
-        sudo -u vagrant -H git clone $g_ANSIBLE_REPOSITORY_URL
+        sudo -u vagrant -H git clone $ansible_repository_url
         popd
     fi
 
     # Update to the latest development version
-    pushd $g_ANSIBLE_DIRECTORY
+    pushd $ansible_directory
     sudo -u vagrant -H git pull
     sudo -u vagrant -H git submodule update --init --recursive
     #shellcheck disable=SC1091
     popd
 
     # Source the development Ansible environment on login
-    source_oneliner="source $g_ANSIBLE_DIRECTORY/hacking/env-setup"
-    bashrc=/home/vagrant/.bash_profile
-    if ! grep "$source_oneliner" $bashrc; then
-        echo "$source_oneliner" >> $bashrc
+    env_oneliner="source $ansible_directory/hacking/env-setup"
+    profile=/home/vagrant/.bash_profile
+    if ! grep "$env_oneliner" $profile; then
+        echo "$env_oneliner" >> $profile
     fi
 }
 
-set_ansible_config() {
-    section=$1
-    option=$2
-    value=$3
-
-    ansible localhost -c local -m ini_file \
-        -a "dest=$g_ANSIBLE_CONFIG section='$section' option='$option' value='$value'"
-}
-
-install_ansible
-mkdir -p /etc/ansible
-touch g_ANSIBLE_CONFIG
-set_ansible_config defaults host_key_checking False
-set_ansible_config defaults inventory /vagrant/inventory.ini
-set_ansible_config defaults remote_user vagrant
-
-# Install Vagrant insecure keypair
-g_VAGRANT_INSECURE_PUBLIC_KEY_URL='https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub'
-g_VAGRANT_INSECURE_PRIVATE_KEY_URL='https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant'
-g_VAGRANT_SSH_DIRECTORY=/home/vagrant/.ssh
-g_VAGRANT_PUBLIC_KEY=$g_VAGRANT_SSH_DIRECTORY/id_rsa.pub
-g_VAGRANT_PRIVATE_KEY=$g_VAGRANT_SSH_DIRECTORY/id_rsa
 
 download_vagrant_key() {
     url=$1
@@ -66,7 +53,19 @@ download_vagrant_key() {
     fi
 }
 
-download_vagrant_key $g_VAGRANT_INSECURE_PUBLIC_KEY_URL $g_VAGRANT_PUBLIC_KEY
-download_vagrant_key $g_VAGRANT_INSECURE_PRIVATE_KEY_URL $g_VAGRANT_PRIVATE_KEY
+download_vagrant_keys() {
+    # Install Vagrant insecure keypair
+    vagrant_insecure_public_key_url='https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub'
+    vagrant_insecure_private_key_url='https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant'
+    vagrant_ssh_directory=/home/vagrant/.ssh
+    vagrant_public_key=$vagrant_ssh_directory/id_rsa.pub
+    vagrant_private_key=$vagrant_ssh_directory/id_rsa
 
-chmod 0600 $g_VAGRANT_PRIVATE_KEY
+    download_vagrant_key $vagrant_insecure_public_key_url $vagrant_public_key
+    download_vagrant_key $vagrant_insecure_private_key_url $vagrant_private_key
+
+    chmod 0600 $vagrant_private_key
+}
+
+install_ansible
+download_vagrant_keys
